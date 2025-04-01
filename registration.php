1c6e34192f48fc5e9abd7272d76c60b811576d54
stvujic <83906734+stvujic@users.noreply.github.com>
2025-04-01 09:17:02 +0200

    Registration and login design

--- registration.php
+++ registration.php
@@ -1,17 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Registration</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
 
-    <body>
-        <form action="model/registrationUser.php" method="POST">
-            <input type="email" name="email" placeholder="Enter your email">
-            <input type="password" name="password" placeholder="Enter your password">
-            <button>Register</button>
+<body>
+<nav>
+        <div class="logo">LOGO</div>
+        <ul class="nav-links">
+            <li><a href="index.php#about">About Us</a></li>
+            <li><a href="index.php#contact">Contact</a></li>
+            <li><a href="products.php">Shop</a></li>
+            <?php if(isset($_SESSION['role']) && $_SESSION['role'] == "admin"): ?>
+                <li><a href="new_product.php">Add Product</a></li>
+                <li><a href="user_search.php">User Search</a></li>     
+            <?php endif; ?> 
+            
+            
+            <?php if(isset($_SESSION['loggedIn'])): ?>
+                <li><a href="logout.php" class="btn logout-btn">Logout</a></li>
+                <li><a href="my_cart.php">Cart</a></li>
+            <?php else:  ?>
+                <li><a href="login.php" class="btn login-btn">Login</a></li>
+                <li><a href="registration.php" class="btn register-btn">Registration</a></li>
+            <?php endif;  ?>
+
+        </ul>
+    </nav>
+
+    <div class="registration-container">
+        <form action="model/registrationUser.php" method="POST" class="registration-form">
+            <h2>Register</h2>
+            <input type="email" name="email" placeholder="Enter your email" required>
+            <input type="password" name="password" placeholder="Enter your password" required>
+            <button type="submit">Register</button>
         </form>
-    </body>
+    </div>
+</body>
+
 </html>
